87199ac17fe9be0ad29a71ba74d90536019cf3be
ardana.mujua21 <cardamu02@gmail.com>
2022-01-25 14:25:08 +0100

    Avances

---
+++ Spring-Boot-Blog-REST-API/src/test/java/com/sopromadze/blogapi/repository/PhotoTest.java
@@ -0,0 +1,62 @@
+package com.sopromadze.blogapi.repository;
+
+import com.sopromadze.blogapi.model.Album;
+import com.sopromadze.blogapi.model.Photo;
+import com.sopromadze.blogapi.model.user.User;
+import com.sopromadze.blogapi.payload.PagedResponse;
+import com.sopromadze.blogapi.utils.AppConstants;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.*;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.Instant;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class PhotoTest {
+    @Autowired
+    private PhotoRepository repository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    void testRepoNotNull() {
+        assertNotNull(repository);
+    }
+
+    @Test
+    void test_findByAlbumId() {
+
+        Album album = new Album();
+        album.setId(1L);
+        album.setTitle("Fotos");
+        album.setCreatedAt(Instant.now());
+        album.setUpdatedAt(Instant.now());
+
+        testEntityManager.persist(album);
+
+
+        Photo photo = new Photo();
+        photo.setId(1L);
+        photo.setTitle("Photo1");
+        photo.setThumbnailUrl("https//photos");
+        photo.setAlbum(album);
+
+        testEntityManager.persist(photo);
+
+
+        Pageable pageable = PageRequest.of(1, 1, Sort.Direction.DESC, AppConstants.CREATED_AT);
+
+        assertNotEquals(0, repository.findByAlbumId(album.getId(), pageable).get().findAny());
+
+    }
+}
