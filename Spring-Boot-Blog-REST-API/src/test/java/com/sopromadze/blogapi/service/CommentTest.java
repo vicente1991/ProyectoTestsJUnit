0bc270982528a30550421198a1032a9eb7742d0b
vicente <etenciv@gmail.com>
2022-01-26 13:27:31 +0100

    mas test :(

--- Spring-Boot-Blog-REST-API/src/test/java/com/sopromadze/blogapi/service/CommentTest.java
+++ Spring-Boot-Blog-REST-API/src/test/java/com/sopromadze/blogapi/service/CommentTest.java
@@ -2,7 +2,10 @@ package com.sopromadze.blogapi.service;
 
 import com.sopromadze.blogapi.model.Comment;
 import com.sopromadze.blogapi.model.Post;
+import com.sopromadze.blogapi.model.role.Role;
+import com.sopromadze.blogapi.model.role.RoleName;
 import com.sopromadze.blogapi.model.user.User;
+import com.sopromadze.blogapi.payload.ApiResponse;
 import com.sopromadze.blogapi.payload.CommentRequest;
 import com.sopromadze.blogapi.repository.CommentRepository;
 import com.sopromadze.blogapi.repository.PostRepository;
@@ -16,9 +19,13 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
@@ -41,9 +48,37 @@ public class CommentTest {
     CommentServiceImpl commentService;
 
 
+    @Test
+    void getComments_Success(){
+
+        User u= new User();
+        u.setFirstName("Vicente");
+        u.setLastName("Rufo Bru");
+        u.setEmail("Vicente@mail.com");
+        u.setUsername("Vicent");
+
+        Post p= new Post();
+        p.setTitle("Post General");
+        p.setId(1L);
+        p.setBody("Explicando cosas");
+        p.setUser(u);
+
+        Comment c= new Comment();
+        c.setName("Comentario 1");
+        c.setId(1L);
+        c.setPost(p);
+        c.setEmail("Vicente@mail.com");
+        c.setBody("Hola que tal");
+        c.setUser(u);
+
+        lenient().when(postRepository.findById(p.getId())).thenReturn(java.util.Optional.of(p));
+        lenient().when(commentRepository.findById(c.getId())).thenReturn(java.util.Optional.of(c));
+        assertEquals(c,commentService.getComment(p.getId(),c.getId()));
+
+    }
 
     @Test
-    public void whennewComment_Succes(){
+    public void whennewComment_Success(){
 
         User u= new User();
         u.setFirstName("Vicente");
@@ -67,7 +102,7 @@ public class CommentTest {
         CommentRequest com = new CommentRequest();
                 com.setBody("nuevo mensaje de texto");
 
-        when(userRepository.getUser(up)).thenReturn(u);
+        lenient().when(userRepository.getUser(up)).thenReturn(u);
 
         Comment c= new Comment();
         c.setName("Comentario 1");
@@ -82,8 +117,52 @@ public class CommentTest {
     }
 
     @Test
-    void deleteComment_Succes(){
+    void deleteComment_Success(){
 
+       Role rol = new Role();
+       rol.setName(RoleName.ROLE_ADMIN);
+        List<Role> roles= Arrays.asList(rol);
+        User u= new User();
+        u.setId(1L);
+        u.setFirstName("Vicente");
+        u.setLastName("Rufo Bru");
+        u.setRoles(roles);
+        u.setEmail("Vicente@mail.com");
+        u.setUsername("Vicent");
+
+
+        UserPrincipal up= UserPrincipal.builder()
+                .id(2L)
+                .email("blabla@mail.com")
+                .authorities( u.getRoles().stream()
+                        .map(role -> new SimpleGrantedAuthority(role.getName().name())).collect(Collectors.toList()))
+                .firstName("bla")
+                .lastName("blabla")
+                .build();
+
+        Post p= new Post();
+        p.setTitle("Post General");
+        p.setId(1L);
+        p.setBody("Explicando cosas");
+        p.setUser(u);
+
+        lenient().when(userRepository.getUser(up)).thenReturn(u);
+
+        CommentRequest com = new CommentRequest();
+        com.setBody("nuevo mensaje de texto");
+
+        Comment c= new Comment();
+        c.setName("Comentario 1");
+        c.setId(1L);
+        c.setPost(p);
+        c.setEmail("Vicente@mail.com");
+        c.setBody("Hola que tal");
+        c.setUser(u);
+
+        lenient().when(postRepository.findById(p.getId())).thenReturn(java.util.Optional.of(p));
+        lenient().when(commentRepository.findById(c.getId())).thenReturn(java.util.Optional.of(c));
+        commentService.deleteComment(c.getId(),c.getId(),up);
+        assertEquals(true,up.getAuthorities().contains(new SimpleGrantedAuthority(RoleName.ROLE_ADMIN.toString())));
 
     }
 
