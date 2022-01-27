6d3dfab3363fda4e57623be8b5cd3bf5ad365bb0
María Inmaculada Domínguez Vargas <onemayn@outlook.es>
2022-01-27 12:55:38 +0100

    Hay tests que corregir. Enseñadle a Luismi el test getAlbum

--- Spring-Boot-Blog-REST-API/src/test/java/com/sopromadze/blogapi/service/impl/AlbumServiceImplTest.java
+++ Spring-Boot-Blog-REST-API/src/test/java/com/sopromadze/blogapi/service/impl/AlbumServiceImplTest.java
@@ -1,11 +1,13 @@
 package com.sopromadze.blogapi.service.impl;
 
+import com.sopromadze.blogapi.exception.BlogapiException;
 import com.sopromadze.blogapi.exception.ResourceNotFoundException;
 import com.sopromadze.blogapi.model.Album;
 import com.sopromadze.blogapi.model.role.Role;
 import com.sopromadze.blogapi.model.role.RoleName;
 import com.sopromadze.blogapi.model.user.User;
 import com.sopromadze.blogapi.payload.AlbumResponse;
+import com.sopromadze.blogapi.payload.ApiResponse;
 import com.sopromadze.blogapi.payload.request.AlbumRequest;
 import com.sopromadze.blogapi.repository.AlbumRepository;
 import com.sopromadze.blogapi.repository.UserRepository;
@@ -18,6 +20,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.ArrayList;
@@ -46,25 +49,37 @@ class AlbumServiceImplTest {
     @Mock
     UserRepository userRepository;
 
-    static Album album;
-    static User user;
+    static Album album, album2;
+    static User user, user2, user3;
     static UserPrincipal userPrincipal;
-    static AlbumRequest albumRequest;
+    static AlbumRequest albumRequest, albumRequest2;
     static final String CREATED_AT = "createdAt";
     static final String ALBUM_STR = "Album";
-    static List<Role> roles;
+    static final String YOU_DON_T_HAVE_PERMISSION_TO_MAKE_THIS_OPERATION = "You don't have permission to make this operation";
+    static BlogapiException blogapiException;
+    static List<Role> roles, roles2;
+    static List<Album> listaAlbumes;
 
     @BeforeEach
     void initData () {
 
         user = new User ("Inmaculada", "Domínguez", "inmadv", "inma.dvgs@gmail.com", "12345");
+        user2 = new User ("Javier", "Domínguez", "javidv", "javierdominguez2006@gmail.com", "54321");
+        user3 = new User ("Auxiliadora", "Vargas", "auxivm", "auxivargasmedina1969@gmail.com", "54321");
 
         user.setId(3L);
-        roles = new ArrayList<Role>();
+        user2.setId(4L);
+        user3.setId(6L);
 
+        roles = new ArrayList<Role>();
         roles.add(new Role(RoleName.ROLE_ADMIN));
 
+        roles2 = new ArrayList<Role>();
+        roles2.add(new Role(RoleName.ROLE_USER));
+
         user.setRoles(roles);
+        user2.setRoles(roles2);
+        user3.setRoles(roles2);
 
         album = Album.builder()
                 .id(1L)
@@ -72,7 +87,22 @@ class AlbumServiceImplTest {
                 .user(user)
                 .build();
 
+        album2 = Album.builder()
+                .id(5L)
+                .title("Love Yourself : Answer")
+                .user(user2)
+                .build();
+
+
         albumRequest = AlbumRequest.builder().user(user).build();
+        albumRequest2 = AlbumRequest.builder().user(user2).build();
+
+
+        listaAlbumes = new ArrayList<>();
+        listaAlbumes.add(album);
+        user2.setAlbums(listaAlbumes);
+
+        blogapiException = new BlogapiException(HttpStatus.UNAUTHORIZED, YOU_DON_T_HAVE_PERMISSION_TO_MAKE_THIS_OPERATION);
     }
 
     @Test
@@ -101,7 +131,7 @@ class AlbumServiceImplTest {
     @Test
     void doesNotGetAlbum(){
         when(albumRepository.findById(44L)).thenThrow(new ResourceNotFoundException(ALBUM_STR, ID, 44L));
-
+        assertThat(new ResourceNotFoundException(ALBUM_STR, ID, 44L));
     }
 
     @Test
@@ -135,8 +165,22 @@ class AlbumServiceImplTest {
     }
 
     @Test
-    void deleteAlbum() {
+    void notAdmin_updateAlbum(){
+        assertThat(albumService.updateAlbum(album2.getId(), albumRequest2, userPrincipal.create(user2)).equals(blogapiException));
     }
+
+    @Test
+    void deletesAlbum() {
+        ApiResponse result = new ApiResponse(Boolean.TRUE, "You successfully deleted album");
+        assertEquals(result, albumService.deleteAlbum(1L, userPrincipal.create(user)));
+
+    }
+
+    @Test
+    void unauthorizedUser_deleteAlbum(){
+        assertEquals(albumService.deleteAlbum(1L, userPrincipal.create(user3)), blogapiException);
+    }
+
 
     @Test
     void getUserAlbums() {
