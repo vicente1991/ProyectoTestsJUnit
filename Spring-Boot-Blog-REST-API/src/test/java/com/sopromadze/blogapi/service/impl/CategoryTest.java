2941d22efe95f7eebd58bf94aa4177a453ec6b2b
Juan Carlos Ardana Murillo <cardamu02@gmail.com>
2022-01-30 18:01:30 +0100

    arreglo de codigo

--- Spring-Boot-Blog-REST-API/src/test/java/com/sopromadze/blogapi/service/impl/CategoryTest.java
+++ Spring-Boot-Blog-REST-API/src/test/java/com/sopromadze/blogapi/service/impl/CategoryTest.java
@@ -13,6 +13,7 @@ import com.sopromadze.blogapi.payload.ApiResponse;
 import com.sopromadze.blogapi.payload.PagedResponse;
 import com.sopromadze.blogapi.repository.CategoryRepository;
 import com.sopromadze.blogapi.security.UserPrincipal;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -42,70 +43,86 @@ public class CategoryTest {
     @InjectMocks
     CategoryServiceImpl categoryService;
 
-    @Test
-    void getAllCategories_success(){
+    User user;
+    List<Post> postList;
+    Category category;
+    Page<Category> pageResult;
+    PagedResponse<Category> result;
+    Pageable pageable;
 
+    Role rolAdmin;
+    Role rolUser;
 
-        User user = new User();
+    Category newCategory;
+
+    @BeforeEach
+    void initTest() {
+        user =  new User();
         user.setUsername("user");
         user.setId(1L);
 
-        List<Post> postList = new ArrayList<>();
+        postList = new ArrayList<>();
 
-        Category category = new Category();
+        category = new Category();
         category.setId(1L);
         category.setPosts(postList);
         category.setName("Categoria");
 
-        Page<Category> pageResult = new PageImpl<>(Arrays.asList(category));
+        pageResult = new PageImpl<>(Arrays.asList(category));
 
-        PagedResponse<Category> result = new PagedResponse<>();
+        result = new PagedResponse<>();
         result.setContent(pageResult.getContent());
         result.setTotalPages(1);
         result.setTotalElements(1);
         result.setLast(true);
         result.setSize(1);
 
-        Pageable pageable = PageRequest.of(1, 1, Sort.Direction.DESC, "createdAt");
+        pageable = PageRequest.of(1, 1, Sort.Direction.DESC, "createdAt");
 
-        when(categoryRepository.findAll(pageable)).thenReturn(pageResult);
+        rolAdmin = new Role();
+        rolAdmin.setName(RoleName.ROLE_ADMIN);
 
-        assertEquals(result,categoryService.getAllCategories(1,1));
+        rolUser = new Role();
+        rolUser.setName(RoleName.ROLE_USER);
+
+        newCategory = new Category();
+        newCategory.setId(1L);
+        newCategory.setPosts(postList);
+        newCategory.setName("Categoria nueva");
+
     }
 
+    @Test
+    void getAllCategories_success(){
 
+        when(categoryRepository.findAll(pageable)).thenReturn(pageResult);
+        assertEquals(result,categoryService.getAllCategories(1,1));
 
+    }
 
 
     @Test
     void getCategory_success(){
 
-        List<Post> postList = new ArrayList<>();
-
-        Category category = new Category();
-        category.setId(1L);
-        category.setPosts(postList);
-        category.setName("Categoria");
-
         when(categoryRepository.findById(1L)).thenReturn(Optional.of(category));
         assertEquals(category,categoryService.getCategory(1L));
+
     }
+
+
     @Test
     void getCategory_throwResourceNotFoundException(){
+
         when(categoryRepository.findById(any(Long.class))).thenReturn(Optional.empty());
         assertThrows(ResourceNotFoundException.class,()->categoryService.getCategory(any(Long.class)));
+
     }
 
 
     @Test
     void addCategory_success(){
 
-        Role rol = new Role();
-        rol.setName(RoleName.ROLE_ADMIN);
-
-        List<Role> roles = Arrays.asList(rol);
-
-        User user = new User();
+        List<Role> roles = Arrays.asList(rolAdmin);
         user.setId(3L);
         user.setRoles(roles);
 
@@ -114,14 +131,6 @@ public class CategoryTest {
                 .authorities(user.getRoles().stream()
                         .map(role -> new SimpleGrantedAuthority(role.getName().name())).collect(Collectors.toList()))
                 .build();
-
-
-        List<Post> postList = new ArrayList<>();
-
-        Category category = new Category();
-        category.setId(1L);
-        category.setPosts(postList);
-        category.setName("Categoria");
 
         when(categoryRepository.save(category)).thenReturn(category);
         assertEquals(category,categoryService.addCategory(category, userPrincipal));
@@ -131,12 +140,7 @@ public class CategoryTest {
     @Test
     void updateCategory_success(){
 
-        Role rol = new Role();
-        rol.setName(RoleName.ROLE_ADMIN);
-
-        List<Role> roles = Arrays.asList(rol);
-
-        User user = new User();
+        List<Role> roles = Arrays.asList(rolAdmin);
         user.setId(3L);
         user.setRoles(roles);
 
@@ -145,25 +149,13 @@ public class CategoryTest {
                 .authorities(user.getRoles().stream()
                         .map(role -> new SimpleGrantedAuthority(role.getName().name())).collect(Collectors.toList()))
                 .build();
-        List<Post> postList = new ArrayList<>();
 
-
-
-        Category newCategory = new Category();
-        newCategory.setId(1L);
-        newCategory.setPosts(postList);
-        newCategory.setName("Categoria nueva");
-        //newCategory.setCreatedBy(10L);
-
-        Category category = new Category();
         category.setId(4L);
-        category.setPosts(postList);
-        category.setName("Categoria");
         category.setCreatedBy(3L);
 
 
-
         when(categoryRepository.findById(4L)).thenReturn(Optional.of(category));
+
         category.setName(newCategory.getName());
 
         when(categoryRepository.save(category)).thenReturn(category);
@@ -174,12 +166,7 @@ public class CategoryTest {
     @Test
     void updateCategory_throwResourceNotFoundException(){
 
-        Role rol = new Role();
-        rol.setName(RoleName.ROLE_ADMIN);
-
-        List<Role> roles = Arrays.asList(rol);
-
-        User user = new User();
+        List<Role> roles = Arrays.asList(rolAdmin);
         user.setId(3L);
         user.setRoles(roles);
 
@@ -188,18 +175,15 @@ public class CategoryTest {
                 .authorities(user.getRoles().stream()
                         .map(role -> new SimpleGrantedAuthority(role.getName().name())).collect(Collectors.toList()))
                 .build();
+
+
         when(categoryRepository.findById(any(Long.class))).thenReturn(Optional.empty());
         assertThrows(ResourceNotFoundException.class,()->categoryService.updateCategory(null,any(Category.class),userPrincipal));
     }
     @Test
     void updateCategory_throwUnauthorizedException(){
 
-        Role rol = new Role();
-        rol.setName(RoleName.ROLE_USER);
-
-        List<Role> roles = Arrays.asList(rol);
-
-        User user = new User();
+        List<Role> roles = Arrays.asList(rolUser);
         user.setId(3L);
         user.setRoles(roles);
 
@@ -208,22 +192,14 @@ public class CategoryTest {
                 .authorities(user.getRoles().stream()
                         .map(role -> new SimpleGrantedAuthority(role.getName().name())).collect(Collectors.toList()))
                 .build();
-        List<Post> postList = new ArrayList<>();
 
-        Category newCategory = new Category();
-        newCategory.setId(1L);
-        newCategory.setPosts(postList);
-        newCategory.setName("Categoria nueva");
 
-        Category category = new Category();
         category.setId(4L);
-        category.setPosts(postList);
-        category.setName("Categoria");
         category.setCreatedBy(10L);
 
 
-
         when(categoryRepository.findById(4L)).thenReturn(Optional.of(category));
+
         category.setName(newCategory.getName());
 
         when(categoryRepository.save(category)).thenReturn(category);
@@ -234,12 +210,7 @@ public class CategoryTest {
     @Test
     void deleteCategory_success(){
 
-        Role rol = new Role();
-        rol.setName(RoleName.ROLE_ADMIN);
-
-        List<Role> roles = Arrays.asList(rol);
-
-        User user = new User();
+        List<Role> roles = Arrays.asList(rolAdmin);
         user.setId(3L);
         user.setRoles(roles);
 
@@ -248,21 +219,21 @@ public class CategoryTest {
                 .authorities(user.getRoles().stream()
                         .map(role -> new SimpleGrantedAuthority(role.getName().name())).collect(Collectors.toList()))
                 .build();
-        List<Post> postList = new ArrayList<>();
 
         ApiResponse apiResponse = new ApiResponse();
         apiResponse.setSuccess(true);
         apiResponse.setMessage("You successfully deleted category");
 
-        Category category = new Category();
         category.setId(4L);
-        category.setPosts(postList);
-        category.setName("Categoria");
         category.setCreatedBy(3L);
 
+
         when(categoryRepository.findById(4L)).thenReturn(Optional.of(category));
+
         doNothing().when(categoryRepository).delete(category);
+
         categoryService.deleteCategory(4L,userPrincipal);
+
         assertEquals(apiResponse,categoryService.deleteCategory(category.getId(),userPrincipal));
 
     }
@@ -270,12 +241,7 @@ public class CategoryTest {
     @Test
     void deleteCategory_throwResourceNotFoundException(){
 
-        Role rol = new Role();
-        rol.setName(RoleName.ROLE_ADMIN);
-
-        List<Role> roles = Arrays.asList(rol);
-
-        User user = new User();
+        List<Role> roles = Arrays.asList(rolAdmin);
         user.setId(3L);
         user.setRoles(roles);
 
@@ -284,19 +250,17 @@ public class CategoryTest {
                 .authorities(user.getRoles().stream()
                         .map(role -> new SimpleGrantedAuthority(role.getName().name())).collect(Collectors.toList()))
                 .build();
+
+
         when(categoryRepository.findById(any(Long.class))).thenReturn(Optional.empty());
+
         assertThrows(ResourceNotFoundException.class,()->categoryService.deleteCategory(any(Long.class),userPrincipal));
     }
 
     @Test
     void deleteCategory_throwUnauthorizedException(){
 
-        Role rol = new Role();
-        rol.setName(RoleName.ROLE_USER);
-
-        List<Role> roles = Arrays.asList(rol);
-
-        User user = new User();
+        List<Role> roles = Arrays.asList(rolUser);
         user.setId(3L);
         user.setRoles(roles);
 
@@ -305,22 +269,13 @@ public class CategoryTest {
                 .authorities(user.getRoles().stream()
                         .map(role -> new SimpleGrantedAuthority(role.getName().name())).collect(Collectors.toList()))
                 .build();
-        List<Post> postList = new ArrayList<>();
-
-        Category newCategory = new Category();
-        newCategory.setId(1L);
-        newCategory.setPosts(postList);
-        newCategory.setName("Categoria nueva");
-
-        Category category = new Category();
+        
         category.setId(4L);
-        category.setPosts(postList);
-        category.setName("Categoria");
         category.setCreatedBy(10L);
 
 
-
         when(categoryRepository.findById(4L)).thenReturn(Optional.of(category));
+
         category.setName(newCategory.getName());
 
         when(categoryRepository.save(category)).thenReturn(category);
