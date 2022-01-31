08ff1f940738b544e8c1a55c9930f21251078916
Juan Carlos Ardana Murillo <cardamu02@gmail.com>
2022-02-01 00:39:37 +0100

    Terminado CustomUserDetailsServiceImpl Service Test

--- Spring-Boot-Blog-REST-API/src/test/java/com/sopromadze/blogapi/service/CustomUserDetailsServiceTest.java
+++ Spring-Boot-Blog-REST-API/src/test/java/com/sopromadze/blogapi/service/CustomUserDetailsServiceTest.java
@@ -1,8 +1,11 @@
 package com.sopromadze.blogapi.service;
 
 
+import com.sopromadze.blogapi.model.role.Role;
+import com.sopromadze.blogapi.model.role.RoleName;
 import com.sopromadze.blogapi.model.user.User;
 import com.sopromadze.blogapi.repository.UserRepository;
+import com.sopromadze.blogapi.security.UserPrincipal;
 import com.sopromadze.blogapi.service.impl.CategoryServiceImpl;
 import com.sopromadze.blogapi.service.impl.CustomUserDetailsServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,8 +14,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -29,25 +35,57 @@ public class CustomUserDetailsServiceTest {
 
     User user;
     UserDetails userDetails;
-
+    Role rolAdmin;
+    Role rolUser;
     @BeforeEach
     void initTest() {
+
+
         user =  new User();
         user.setPassword("1234jc");
-        user.setEmail("hola@gmail.com");
+        user.setEmail("user");
         user.setLastName("hola");
         user.setFirstName("hola");
         user.setUsername("user");
         user.setId(1L);
 
-        //userDetails =
+        rolAdmin = new Role();
+        rolAdmin.setName(RoleName.ROLE_ADMIN);
+
+        rolUser = new Role();
+        rolUser.setName(RoleName.ROLE_USER);
+
+        List<Role> roles = Arrays.asList(rolUser);
+        user.setRoles(roles);
+
+        userDetails = UserPrincipal.create(user);
 
     }
 
     @Test
     void loadUserByUsername_success(){
-        when(userRepository.findByUsername(user.getUsername())).thenReturn(Optional.of(user));
-
-        //assertEquals(user, );
+        when(userRepository.findByUsernameOrEmail("user", "user")).thenReturn(Optional.of(user));
+        assertEquals(userDetails, customUserDetailsService.loadUserByUsername(user.getUsername()));
     }
+
+
+    @Test
+    void loadUserByUsername_throwUsernameNotFoundException(){
+        when(userRepository.findByUsernameOrEmail("user2", "user2")).thenReturn(Optional.of(user));
+        assertThrows(UsernameNotFoundException.class,()-> customUserDetailsService.loadUserByUsername(user.getUsername()));
+    }
+
+
+    @Test
+    void loadUserById_success(){
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+        assertEquals(userDetails, customUserDetailsService.loadUserById(user.getId()));
+    }
+
+    @Test
+    void loadUserById_throwUsernameNotFoundException(){
+        when(userRepository.findByUsernameOrEmail("user2", "user2")).thenReturn(Optional.of(user));
+        assertThrows(UsernameNotFoundException.class,()-> customUserDetailsService.loadUserById(user.getId()));
+    }
+
 }
