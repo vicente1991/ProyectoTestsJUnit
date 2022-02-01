bb9ac7212af699c2d162633f5334beccdef4193a
María Inmaculada Domínguez Vargas <dominguez.vamar21@triana.salesianos.edu>
2022-02-01 09:41:10 +0100

    Corrección final AlbumServiceImplTest. Espero que sea la última

---
+++ Spring-Boot-Blog-REST-API/src/test/java/com/sopromadze/blogapi/controller/PostControllerTest.java
@@ -0,0 +1,36 @@
+package com.sopromadze.blogapi.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostControllerTest {
+
+    @Test
+    void getAllPosts() {
+    }
+
+    @Test
+    void getPostsByCategory() {
+    }
+
+    @Test
+    void getPostsByTag() {
+    }
+
+    @Test
+    void addPost() {
+    }
+
+    @Test
+    void getPost() {
+    }
+
+    @Test
+    void updatePost() {
+    }
+
+    @Test
+    void deletePost() {
+    }
+}
